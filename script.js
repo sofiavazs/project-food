b3c214a231cb3353e0cbab8302e6bfa576753a5d
Sofia Sousa <sofiavazsousa@gmail.com>
2020-09-29 19:03:57 +0200

    Add function that builds the restaurant list

--- script.js
+++ script.js
@@ -19,57 +19,59 @@ const apiKey = {
   }
 };
 
+const buildRestaurantList = (data) => {
+  data.restaurants.forEach((restaurantContainer) => {
+    const restaurantList = document.getElementById('restaurantList');
+
+    restaurantList.innerHTML += `
+    <section class="restaurants">
+      <div id="card-info">
+        <p>${restaurantContainer.restaurant.name}</p>
+        <p>${restaurantContainer.restaurant.user_rating.rating_text} ${restaurantContainer.restaurant.user_rating.aggregate_rating}</p>
+      </div>
+      <img id="image"src=${restaurantContainer.restaurant.featured_image}>
+      <div id="cost">
+        <p>Price Range: ${restaurantContainer.restaurant.price_range}</p>
+      </div>
+      <div>
+        <p>Address: ${restaurantContainer.restaurant.location.address}</p>
+      </div>
+    </section>
+    `;
+  });
+
+}
+
+const filterByPriceRange = (priceRangeValue) => {
+  if (priceRangeValue === 1) {
+    console.log(priceRangeValue)
+  } else if (priceRangeValue === 2) {
+    console.log(priceRangeValue)
+  } else if (priceRangeValue === 3) {
+    console.log(priceRangeValue)
+  } else if (priceRangeValue === 4) {
+    console.log(priceRangeValue)
+  } else if (priceRangeValue === 5) {
+    console.log(priceRangeValue)
+  } else {
+    console.log(priceRangeValue)
+  }
+}
+
 fetch(zomatoApiUrlCuisine, apiKey)
   .then((response) => {
     return response.json();
   })
   .then((data) => {
     console.log(data);
-    data.restaurants.forEach((restaurantContainer) => {
-      const restaurantList = document.getElementById('restaurantList');
+    buildRestaurantList(data);
 
-      restaurantList.innerHTML += `
-      <section class="restaurants">
-        <div id="card-info">
-          <p>${restaurantContainer.restaurant.name}</p>
-          <p>${restaurantContainer.restaurant.user_rating.rating_text} ${restaurantContainer.restaurant.user_rating.aggregate_rating}</p>
-        </div>
-        <img id="image"src=${restaurantContainer.restaurant.featured_image}>
-        <div id="cost">
-          <p>Price Range: ${restaurantContainer.restaurant.price_range}</p>
-        </div>
-        <div>
-          <p>Address: ${restaurantContainer.restaurant.location.address}</p>
-        </div>
-      </section>
-      `;
-
-
-    });
-    const filterPriceRange = restaurantContainer.restaurant.price_range;
-    const priceRangeValue = document.getElementById("priceRanges").value
-    const priceRangeElement = (filterPriceRange, priceRangeValue) => {
-      if (filterPriceRange === 1) {
-        // console.log("$")
-      } else if (filterPriceRange === 2) {
-        // console.log(filterPriceRange)
-      } else if (filterPriceRange === 3) {
-        // console.log(filterPriceRange)
-      } else if (filterPriceRange === 4) {
-        // console.log(filterPriceRange)
-      } else if (filterPriceRange === 5) {
-        // console.log(filterPriceRange)
-      } else {
-        // console.log(filterPriceRange)
-      }
-    }
-    let selectPriceRange = '';
-    priceRangeElement(selectPriceRange);
+    // const filterPriceRange = restaurantContainer.restaurant.price_range; // don't think we need this
+    // const priceRangeValue = document.getElementById("priceRanges").value //same here! 
 
     document.getElementById('priceRanges').addEventListener('change', (event) => {
-      selectPriceRange = event.target.value;
-      console.log(selectPriceRange)
-      priceRangeElement(filterPriceRange, priceRangeValue);
-
+      const selectedPriceRangeValue = event.target.value;
+      console.log(selectedPriceRangeValue)
+      filterByPriceRange(selectedPriceRangeValue);
     });
   });
