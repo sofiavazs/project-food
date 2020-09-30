c200919f30d88fab8912ccbecc984d312fea61f0
Sofia Sousa <sofiavazsousa@gmail.com>
2020-09-30 21:07:31 +0200

    Add filter by review feature + star rating + dollar sign rating

--- README.md
+++ README.md
@@ -1,82 +1,19 @@
-# Build a Food Review website 🌮🥑
+# Food Review website 🌮🥑
 
-During this week we will finally have a remote collaborative session – We can't wait! During those days we will practice our JavaScript and API skills a bit more. We will also practice working as a real team using GitHub. You will experience, first-hand, the issues and struggles that can occur when working together with code. 
+Website that lists restaurants, reviews and information based on location – YUM 😋. 
 
-During the session your team will be challenged to build a website that lists restaurants, reviews and information based on location – YUM 😋. 
-
-Your page will be built based on a random combination of cuisine, city, and design provided by the Technigo team on site. All data should be fetched from the [Zomato API.](https://developers.zomato.com/)  
-
-## Your team's combination: Burgers restaurants in Sydney
-
-Your team has been tasked with using the Zomato API to list Burgers restaurants in Sydney in the style of the Online Pizza site:
-![Online Pizza](https://camo.githubusercontent.com/b72485a96a0e7cb1d5487342b2712593ce00f405/68747470733a2f2f692e696d6775722e636f6d2f435668337a69302e6a7067)
-
-## How to complete this task
-
-We have a very limited time-frame for this project, so we want you to read about the Zomato API and experiment making requests to the `/search` endpoint before we meet up. This way, when you all meet in Stockholm and get assigned your cuisine, location and design, you'll be ready to get going quickly. 
-
-*The following steps should be done during the meet-up:* 
-
-### Step 1
-
-Each member of your team should clone (NOT FORK!) the correct repo for your team.
-
-### Step 2
-
-Togeteher as a mob, do the foundations of the project together. Make sure that you have fetched from the API and presented all data on the page in a suitable HTML structure which you all agree on. You can also use one person's code from the practices as long as **everyone** understands the code. No styling is needed at this point. 
-
-You should now have data from at least 10 restaurants from the `/search` endpoint on your page, and include the following data: 
-
-* The restaurant name
-* The average cost for a dinner there
-* The address of the restaurant
-* An image (you choose which image you'd like to display from the response)
-* Either the `aggregate_rating` or the `rating_text` for that restaurant
-
-### Step 3 
-
-Divide into pairs or groups of three and choose freely from the selection of features below. The groups will work separatley on features in their own "feature branch" and we will help you to merge this into the master branch before the demo. Of course; have communication within the group and feel free to merge and commit code when you feel that the time is right. Help each-other and share knowledge in the team is always encouraged. 
+Page built to fetch burgers restaurants in Sydney using the Zomato API. 
 
 ### Features
 
-Below are suggestions for features for your site. You're free to choose the ones which are interesting to your team to implement: 
-
-**🎨Implement the randomly given design.**
-
-You don't need to implement all sections (if there's any that is not relevant to your site) but try and copy the page look and feel and design details. Feel free to make text, logo and other things your own – it's a tribute, not an exact copy.
-
 **💰Filter on price range** 
 
-Add so that your users can filter restaurants based on different price ranges. 
-
-**🚀Sort on price** 
-
-Add so that your users can sort the resturants on price, cheap to expensive using the `average_cost_for_two` parameter. 
-
-**💬List reviews** 
-
-List the 3 latest reviews for each restaurant. 
-
-**💬Filter on delivery options** 
-
-Make it so your users can choose to only show resturants which have delivery (`has_online_delivery`) or can be booked in advance (`has_table_booking`). 
-
 **🖼Filter on reviews**
+**📱Responsive**
 
-Make it so your users can choose to only show restaurants with good ratings.
+Tech ⚡
+- HTML5
+- CSS3
+- JavaScript
 
-**📱Make the page responsive**
-
-To work on mobile, desktop or tablet. 
-
-Feel free to challenge yourself and team to come up with more or other features! This is your group project – you decided. 
-
-### Feature freeze 
-
-There's a thing called [feature freeze.](https://en.wikipedia.org/wiki/Freeze_(software_engineering)), our feature freeze will be 2 hours before the demo. That's when it's time for you to merge your feature branches into the master branch. Our teachers will have a bit of a though time, since it's guaranteed there will be conflicts in your code (which happens when the same code is edited in two different branches). So expect merge conflicts at this point!
-
-Once all the feature branches are merged into master, you're free to try and make smaller changes to your site and fix bugs - but be careful to make sure it'll be demoable. 
-
-Work as a team!  
-
-Good luck! 
+View it live
--- script.js
+++ script.js
@@ -1,38 +1,108 @@
-// You should now have data from *at least* 10 restaurants 
-//from the `/search` endpoint on your page, 
-//and include the following data:
+let zomatoData; //global variable 
 
-// - The restaurant name
-// - The average cost for a dinner there
-// - The address of the restaurant
-// - An image (you choose which image you'd like to display from the response)
-// - Either the `aggregate_rating` or the `rating_text` for that restaurant
-let zomatoData;
+//filters variables based on the default values
+const noPriceRange = "0";
+const noReviewRating = "no-review";
 
-const buildRestaurantList = (data, priceRangeValue) => {
-  const restaurantList = document.getElementById('restaurantList');
-  restaurantList.innerHTML = "";
-  data.restaurants.forEach((restaurantContainer) => {
-    if (priceRangeValue && priceRangeValue != restaurantContainer.restaurant.price_range) {
-      return 
-    }
-    restaurantList.innerHTML += `
+// Registers active filters 
+
+let activeFilters = {
+  priceRange: noPriceRange,
+  reviewRating: noReviewRating,
+};
+
+// loop that adds a $ sign as many times as the number in the restaurant's price range  
+
+const priceRangeText = (priceRange) => {
+  // if (priceRange === 1) {
+  //   return "$"
+  // }
+  // else if (priceRange === 2) {
+  //   return "$$"
+  // }
+  // else if (priceRange === 3) {
+  //   return "$$$"
+  // }
+  // else if (priceRange === 4) {
+  //   return "$$$$"
+  // }
+  // else if (priceRange === 5) {
+  //   return "$$$$$"
+  // }
+  // else {
+  //   return "N/A"
+  // }
+
+  // does the same as the function with "if statements"
+  let txt = "";
+  for (let i = 0; i < priceRange; i++) {
+    txt += "$";
+  }
+  return txt;
+}
+
+// Function to show the star rating with using same method as above (for loop)
+const ratingStar = (starRating) => {
+  let star = "";
+  for (let i = 0; i < starRating; i++) {
+    star += "<span class=\"fa fa-star checked\"</span>"
+  }
+  return star;
+}
+
+// Function that injects restaurants in the list 
+
+const insertRestaurantInList = (restaurant, element) => {
+  const priceRange = priceRangeText(restaurant.price_range);
+  const starRating = ratingStar(restaurant.user_rating.aggregate_rating);
+
+  element.innerHTML += `
     <section class="restaurants">
       <div id="card-info">
-        <p class="restaurantName">${restaurantContainer.restaurant.name}</p>
-        <p>${restaurantContainer.restaurant.user_rating.rating_text} ${restaurantContainer.restaurant.user_rating.aggregate_rating}</p>
+        <p class="restaurantName">${restaurant.name}</p>
+        <p>${restaurant.user_rating.rating_text} ${starRating}</p>
       </div>
-      <img id="image"src=${restaurantContainer.restaurant.featured_image}>
+      <img id="image"src=${restaurant.featured_image}>
       <div id="cost">
-        <p class ="dollar">Price Range: ${restaurantContainer.restaurant.price_range}</p>
-        <p>Average cost (2 people): $${restaurantContainer.restaurant.average_cost_for_two}</p>
+        <p class ="dollar"><span style="color:green">${priceRange}</span></p>
+        <p>Average cost (2 people): $${restaurant.average_cost_for_two}</p>
         <p>
       </div>
       <div>
-        <p>Address: ${restaurantContainer.restaurant.location.address}</p>
+        <p>Address: ${restaurant.location.address}</p>
       </div>
     </section>
-    `;
+  `;
+}
+
+// filter functions 
+function includeRestaurant(filters, restaurant) {
+  // restaurant price range is NOT WITHIN the filtered price range
+  if (filters.priceRange != noPriceRange && filters.priceRange != restaurant.price_range) {
+    return false;
+  }
+
+  // restaurant review rating is NOT WITHIN the filtered review rating
+  if (filters.reviewRating != noReviewRating && filters.reviewRating != restaurant.user_rating.rating_text) {
+    return false;
+  }
+
+  // Respects all the filters
+  return true;
+}
+
+// Loop function over the array of restaurants
+
+const buildRestaurantList = (data, activeFilters) => {
+  const restaurantListElement = document.getElementById('restaurantList');
+  restaurantListElement.innerHTML = "";
+
+  data.restaurants.forEach((restaurantContainer) => {
+    const restaurant = restaurantContainer.restaurant
+
+    if (includeRestaurant(activeFilters, restaurant)) {
+      insertRestaurantInList(restaurant, restaurantListElement);
+    }
   });
 };
 
@@ -51,18 +121,20 @@ const fetchZomato = () => {
     })
     .then((data) => {
       zomatoData = data
-      buildRestaurantList(data);
+      buildRestaurantList(data, activeFilters);
   });
 }
 fetchZomato();
 
-// Filter by price function 
-  
-const dropdownEventListener = document.getElementById('priceRanges').addEventListener;
+// functions responsible for the dropdowns
 
+const priceRangeDropdownEventListener = document.getElementById('priceRanges').addEventListener('change', (event) => {
+  activeFilters.priceRange = event.target.value
+  buildRestaurantList(zomatoData, activeFilters);
+});
 
-dropdownEventListener('change', (event) => {
-  const selectedPriceRangeValue = event.target.value;
-  buildRestaurantList(zomatoData,selectedPriceRangeValue)
+const ratingDropdownEventListener = document.getElementById('rating').addEventListener('change', (event) => {
+  activeFilters.reviewRating = event.target.value
+  buildRestaurantList(zomatoData, activeFilters);
 });
 
